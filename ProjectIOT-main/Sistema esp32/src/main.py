1def702be93ae5a23323a81b441b733d837eef3b
JohnWilliam24dev <johnwilliam24.dev@gmail.com>
2025-03-31 20:20:20 -0300

    python

--- ProjectIOT-main/Sistema esp32/src/main.py
+++ ProjectIOT-main/Sistema esp32/src/main.py
@@ -2,54 +2,60 @@ from machine import Pin
 import time
 import network
 import urequests
-FIREBASE_URL = "https://iotdatabase-6d965-default-rtdb.firebaseio.com/"
-FIREBASE_SECRET = "AIzaSyCMOLltUbE2ji2Y9mddGf87L9fclkAD5Lg"  
-led= Pin(15,Pin.OUT)
+import dht
 
+FIREBASE_URL = "https://sensortemperatura-71222-default-rtdb.firebaseio.com/"
+FIREBASE_SECRET = "AIzaSyCZKDE_augJEXOPiFqCnM497iy5O31Vn-o"
+
+sensor = dht.DHT22(Pin(4))  # Adaptado para o pino 15
+led = Pin(2, Pin.OUT)  # LED para status
 
 def conectar_wifi():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect('Wokwi-GUEST', '')
-
+    
     time.sleep(20)
     if wlan.isconnected():
         print("Conectado:", wlan.ifconfig())
     else:
-        print("debug2") 
+        print("Falha na conexão WiFi")
 
-#if wlan.isconnected():
-    #led= Pin(21,Pin.OUT)
-
-    #while True:
-        #led.value(1)
-   
-def enviar_para_firebase(dado):
-    url = f"https://iotdatabase-6d965-default-rtdb.firebaseio.com//dados.json?auth=AIzaSyCMOLltUbE2ji2Y9mddGf87L9fclkAD5Lg"
+def enviar_para_firebase(temp, hum):
+    url = f"{FIREBASE_URL}/sensor.json?auth={FIREBASE_SECRET}"
+    dado = {"temperatura": temp, "umidade": hum}
     resposta = urequests.patch(url, json=dado)
     print("Resposta Firebase:", resposta.text)
     resposta.close()
 
 def get_firebase():
-    url = f"{FIREBASE_URL}status.json?auth={FIREBASE_SECRET}"
+    url = f"{FIREBASE_URL}/status.json?auth={FIREBASE_SECRET}"
     response = urequests.get(url)
     if response.status_code == 200:
-
-        data = response.text
+        data = response.json()
         print("Dados recebidos:", data)
         return data.get("status")
     else:
         print("Erro ao obter dados:", response.status_code)
         return False
     response.close()
-   
 
-# Exemplo de envio de dado
+# Conectar ao WiFi
 conectar_wifi()
-#enviar_para_firebase({"temperatura": 25, "umidade": 60})
-#get_firebase()
+
 while True:
-    if get_firebase()=="True":
+    
+    try:
         led.value(1)
-    elif get_firebase()=="False":
-        led.value(0)
+        sensor.measure()
+        temp = sensor.temperature()
+        hum = sensor.humidity()
+        #temp =2
+        #hum=80
+        print(f"Temperatura: {temp}°C, Umidade: {hum}%")
+        enviar_para_firebase(temp, hum)
+    except Exception as e:
+        print("Erro ao ler/enviar dados:", e)
+    
+    
+    time.sleep(5)
